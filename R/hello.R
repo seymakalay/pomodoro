94def5dbae1607dea3e2c052743ae14eabff54f7
seyma kalay <seymakalay@hotmail.comgit config --global seymakalay@hotmail.com>
2021-10-24 12:38:12 +0200

    First commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' My Hello Word Function
+#'
+#' @param x The name of the person to say hi to.
+#'
+#' @return The output from  \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Seyma")
+#' \dontrun{
+#' hello("Steve")
+#' }
+hello <- function(x) {
+  print(paste0("Hello ", x, ",this is the world!"))
+}
